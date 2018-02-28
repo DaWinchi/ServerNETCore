0a95b820b0208a78cf596e4a241a67e9c1c5c0aa
prutskov <lehaprutskov@gmail.com>
2018-02-28 22:09:10 +0300

    Объявлен новый класс для кнопки

---
+++ WindowsLibrary/ButtonObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsLibrary
+{
+    class ButtonObject
+    {
+        private int left, top, width, height;
+        private bool isActive;
+
+        public int Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+        public int Top
+        {
+            get { return top; }
+            set { top = value; }
+        }
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public bool IsActive
+        {
+            get { return isActive; }
+            set { isActive = value; }
+        }
+    }
+}
