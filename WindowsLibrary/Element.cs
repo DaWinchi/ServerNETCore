7762dec950e6f7fd823c588936f55ca38770824c
prutskov <lehaprutskov@gmail.com>
2018-03-01 20:26:24 +0300

    Abstract class Element is created

---
+++ WindowsLibrary/Element.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsLibrary
+{
+    public abstract class Element
+    {
+        public virtual int Left { get; set; }
+        public virtual int Top { get; set; }
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+        public bool IsActive { get; set; }
+        public abstract void Update();
+
+    }
+}
