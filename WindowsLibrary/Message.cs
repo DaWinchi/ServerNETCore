3a2e08cadb6837596669e1ff8012323e1bb74b64
prutskov <lehaprutskov@gmail.com>
2018-03-26 16:16:26 +0300

    Добавляю новый тип сообщений на обновление

--- WindowsLibrary/Message.cs
+++ WindowsLibrary/Message.cs
@@ -9,6 +9,13 @@ namespace WindowsLibrary
     {
         public enum KeyPressed { Null, Enter=1, Space=2, Up=3, Down=4, Tab=5 };
         public enum Window { Null, Exit};
+
+        public enum TypeElement {Null, Window, Children};
+        public struct Update
+        {
+            public TypeElement type;
+            public int identificator;
+        }
         public KeyPressed keyPressed;
         public Window window;
 
