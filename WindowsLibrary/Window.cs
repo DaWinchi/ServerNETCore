9868c511cb878b9169b46e0114dae77f956681d1
prutskov <lehaprutskov@gmail.com>
2018-04-25 22:52:10 +0300

    Добавлены xml комментарии к Window

--- WindowsLibrary/Window.cs
+++ WindowsLibrary/Window.cs
@@ -4,13 +4,18 @@ using System.Threading;
 
 namespace WindowsLibrary
 {
+    /// <summary>
+    /// Класс окна
+    /// </summary>
     public class Window : Element
     {
-        /*Приложение, которому принадлежит данное окно*/
+        /// <summary>
+        /// Объект класса приложения, которому принадлежит данное окно
+        /// </summary>
         protected Application ParentApp;
-
-        
-        /*Свойство, отвечающее за активность окна*/
+        /// <summary>
+        ///  Задаёт или получает режим активности окна
+        /// </summary>
         public override bool IsActive
         {
             get => base.IsActive;
@@ -20,32 +25,37 @@ namespace WindowsLibrary
                 ChangeActive(this, new EventArgs());
             }
         }
-
-        /*Свойство, показывающее не закрыто ли окно*/
+        /// <summary>
+        ///  Задаёт или получает информацию о закрытии окна
+        /// </summary>
         public bool IsClosed { get; set; }
-
-        /*Целочисленный идентификатор окна*/
+       /// <summary>
+       /// Задаёт или получает идентификационный номер окна
+       /// </summary>
         public int IdentificationNumber { get; set; }
-
-        /*Набор дочерних элементов*/
+        /// <summary>
+        /// Список дочерних объектов
+        /// </summary>
         public List<Element> Children;
 
 
-
-        /*Событие, возникающее при изменении свойства IsActive окна*/
+        /// <summary>
+        /// Событие, возникающее при изменении состояния активности окна
+        /// </summary>
         protected event EventHandler ChangeActive;
 
 
-        /*Конструктор окна
-         @ p_left - координата левой верхней границы окна по горизонтали
-         @ p_top - координата левой верхней границы окна по вертикали
-         @ p_width - ширина окна
-         @ p_height - высота окна
-         @ p_title - заголовок окна
-         @ p_isactive - флаг активности окна при инициализации(при большом количестве окон в положении true может быть только у одного окна)
-         @ p_identification - целочисленный идентификатор окна
-         @ application - ссылка на приложение, которому принадлежит данное окно
-         */
+        /// <summary>
+        /// Конструктор окна
+        /// </summary>
+        /// <param name="p_left"> горизонтальная координата левого верхнего угла окна </param>
+        /// <param name="p_top"> вертикальная координата левого верхнего угла окна  </param>
+        /// <param name="p_width"> ширина окна </param>
+        /// <param name="p_height"> высота окна </param>
+        /// <param name="p_title"> заголовок окна </param>
+        /// <param name="p_isactive"> активость окна </param>
+        /// <param name="p_identification"> идентификационный номер </param>
+        /// <param name="application"> приложение, которому окно принадлежит </param>
         public Window(int p_left, int p_top, int p_width, int p_height,
             string p_title, bool p_isactive, int p_identification, ref Application application)
         {
@@ -67,26 +77,36 @@ namespace WindowsLibrary
             BackgroundColor = ConsoleColor.Black;
 
         }
-
-
-
+        
         #region Таймер окна
-        /*Сам таёмер*/
+        /// <summary>
+        /// Объект таймера окна
+        /// </summary>
         internal Timer timer;
-        /*Событие "тика" таймера*/
+        /// <summary>
+        /// Событие, возникающее при очередном "тике"
+        /// </summary>
         public event EventHandler TimerTick;
-        /*Функция запуска таймера
-         @timeInterval - интервал срабатывания в миллисекундах*/
+        /// <summary>
+        /// Запускает таймер
+        /// </summary>
+        /// <param name="timeInterval"> шаг таймера в мс </param>
         public void TimerStart(int timeInterval)
         {
             if (timer != null) timer.Dispose();
             timer = new Timer(AddTimerMessage, null, 0, timeInterval);
         }
+        /// <summary>
+        /// Останавливает таймер
+        /// </summary>
         public void TimerStop()
         {
             if (timer != null) timer.Dispose();
         }
-        /*Функция добавляет сообщения таймера в очередь*/
+        /// <summary>
+        /// Добавляет сообщение таймера в очередь сообщений приложения
+        /// </summary>
+        /// <param name="obj"> стандартный параметр функции таймера </param>
         private void AddTimerMessage(object obj)
         {
             Message.TimerMsg msgtimer = new Message.TimerMsg
@@ -97,17 +117,25 @@ namespace WindowsLibrary
             Message msg = new Message { timermsg = msgtimer };
             ParentApp.queue_messages.Enqueue(msg);
         }
-        /*Метод вызывает функцию-событие таймера*/
+        /// <summary>
+        /// Вызывает событие "тика"
+        /// </summary>
         internal void TickFunction()
         {
             TimerTick(this, new EventArgs());
         }
-        /*Функция события, возможно переопределить в дочернем классе*/
+        /// <summary>
+        /// Пустая  функция события
+        /// </summary>
+        /// <param name="sender"> объект, вызвавший событие </param>
+        /// <param name="e"> набор аргументов </param>
         protected virtual void Window_TimerTick(object sender, EventArgs e) { }
 
         #endregion
 
-        /*Метод перерисовывает окно и дочерние элементы*/
+        /// <summary>
+        /// Перерисовывает окно
+        /// </summary>
         internal override void ReDraw()
         {
             CreateFrame();
@@ -115,7 +143,9 @@ namespace WindowsLibrary
             ReDrawChildren();
         }
 
-        /*Метод перерисовывает все дочерние элементы*/
+        /// <summary>
+        /// Перерисовывает дочерние объекты 
+        /// </summary>
         internal override void ReDrawChildren()
         {
             int size = Children.Count;
@@ -125,7 +155,10 @@ namespace WindowsLibrary
             }
         }
 
-        /*Метод ставит в очередь на перерисовку дочерний элемент с индексом в параметре*/
+        /// <summary>
+        /// Ставит в очередь на перерисовку объект с заданным индексом
+        /// </summary>
+        /// <param name="numberElement"> индекс дочернего объекта в списке дочерних объектов </param>
         public void UpdateChildren(int numberElement)
         {
             Message.UpdateElement update = new Message.UpdateElement
@@ -139,7 +172,9 @@ namespace WindowsLibrary
 
         }
 
-        /*Метод ставит в очередь на перерисовку данного окна*/
+        /// <summary>
+        /// Ставит в очередь на перерисовку данное окно
+        /// </summary>
         public void Update()
         {
             Message.UpdateElement update = new Message.UpdateElement
@@ -158,13 +193,19 @@ namespace WindowsLibrary
         }
 
 
-        /*Обработчик события изменения активности окна*/
+        /// <summary>
+        /// Обработчик события изменения состояния активности окна
+        /// </summary>
+        /// <param name="sender"> sender </param>
+        /// <param name="e"> набор аргументов </param>
         protected void Window_ChangeActive(object sender, EventArgs e)
         {
             foreach (Element child in Children) child.IsParentActive = IsActive;
         }
 
-        /*Метод рисует рамку окна*/
+        /// <summary>
+        /// Рисует рамку окна
+        /// </summary>
         protected virtual void CreateFrame()
         {
             Console.BackgroundColor = BackgroundColor;
@@ -208,7 +249,9 @@ namespace WindowsLibrary
             Console.SetCursorPosition(Console.WindowWidth - 2, Console.WindowHeight - 2);
         }
 
-        /*Метод отображает заголовок окна*/
+        /// <summary>
+        /// Отображает заголовок окна
+        /// </summary>
         public virtual void WriteTitle()
         {
             Console.BackgroundColor = BackgroundColor;
@@ -223,8 +266,10 @@ namespace WindowsLibrary
             }
             Console.SetCursorPosition(Console.WindowWidth - 2, Console.WindowHeight - 2);
         }
-
-        /*Метод обработки нажатой на клавиатуре клавиши для данного окна*/
+        /// <summary>
+        ///  Обрабатывает событие нажатия клавиши клавиатуры на окне
+        /// </summary>
+        /// <param name="key">  Информация о нажатой клавише  </param>
         internal override void ReadKey(ConsoleKey key)
         {
             if (IsActive)
@@ -237,13 +282,18 @@ namespace WindowsLibrary
 
         }
 
-        /*Метод добавляет дочерний элемент в окно*/
+        /// <summary>
+        /// Добавляет элемент в список дочерних объектов
+        /// </summary>
+        /// <param name="p_element"> Добавляемый объект </param>
         public override void AddChildren(Element p_element)
         {
             Children.Add(p_element);
         }
 
-        /*Метод добавляет сообщении о закрытии в очередь сообщений приложения*/
+       /// <summary>
+       /// Добавляет сообщение о закрытии окна в очередь сообщений
+       /// </summary>
         public void CloseWindow()
         {
             Message msg = new Message
